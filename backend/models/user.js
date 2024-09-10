51cf7ada54896475da8268c739dac4297267fc78
Samuel Raducan <samuel.thembp@gmail.com>
2024-09-10 15:26:56 +0100

    Created the user schema

---
+++ backend/models/user.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+import bcrpyt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Name is required"],
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+            minLength: [6, "Password must be at least 6 characters long"],
+        },
+        cartItems: [
+            {
+                quantity: {
+                    type: Number,
+                    default: 1,
+                },
+
+                product: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Product",
+                },
+            },
+        ],
+        role: {
+            type: String,
+            enum: ["customer", "admin"],
+            default: "customer",
+        },
+    },
+    // createdAt, updatedAt
+    { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+// Pre-save hook to hash password before saving into the DB
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    try {
+        const salt = await bcrpyt.salt(10);
+        this.password = await bcrpyt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrpyt.compare(password, this.password);
+};
+
+export default User;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server is running on port ${PORT}`);
 });
