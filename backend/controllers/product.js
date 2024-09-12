35b33d72fc29f13ea4310d020c2065e8ce3c8469
Samuel Raducan <samuel.thembp@gmail.com>
2024-09-12 10:21:08 +0100

    Implemented get products by category endpoint

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -118,3 +118,15 @@ export const getRecommendedProducts = async (req, res) => {
         res.status(500).json({ message: "Server error", error: error.message });
     }
 };
+
+export const getProductsByCategory = async (req, res) => {
+    const { category } = req.params;
+
+    try {
+        const products = await Product.find({ category });
+        res.json({ products });
+    } catch (error) {
+        console.log("Error in get products by category controller: ", error);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,6 +5,7 @@ import {
     getFeaturedProducts,
     deleteProduct,
     getRecommendedProducts,
+    getProductsByCategory,
 } from "../controllers/product.js";
 import { adminRoute, protectRoute } from "../middleware/auth.js";
 
@@ -12,6 +13,7 @@ const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
+router.get("/category/:category", getProductsByCategory);
 router.get("/recommendations", getRecommendedProducts);
 router.post("/", protectRoute, adminRoute, createProduct);
 router.delete("/:id", protectRoute, adminRoute, deleteProduct);
