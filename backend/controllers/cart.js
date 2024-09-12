17daa8c37cd7b23a1b166e9433680249cfcc9d3c
Samuel Raducan <samuel.thembp@gmail.com>
2024-09-12 11:35:26 +0100

    Implemented update quantity router and logic

--- backend/controllers/cart.js
+++ backend/controllers/cart.js
@@ -24,8 +24,11 @@ export const removeAllFromCart = async (req, res) => {
         const user = req.user;
 
         if (!productId) user.cartItems = [];
-        else user.cartItems = user.cartItems.filter( (item) => item.id !== productId );
-        
+        else
+            user.cartItems = user.cartItems.filter(
+                (item) => item.id !== productId
+            );
+
         await user.save();
         res.json(user.cartItems);
     } catch (error) {
@@ -33,6 +36,34 @@ export const removeAllFromCart = async (req, res) => {
     }
 };
 
-export const getCartProducts = async (req, res) => {};
+export const updateQuantity = async (req, res) => {
+    try {
+        const { id: productId } = req.params;
+        const { quantity } = req.body;
+        const user = req.user;
+        const existingItem = user.cartItems.find(
+            (item) => item.id === productId
+        );
 
-export const updateQuantity = async (req, res) => {};
+        if (existingItem) {
+            if (quantity === 0) {
+                user.cartItems = user.cartItems.filter(
+                    (item) => item.id !== productId
+                );
+                await user.save();
+                res.json(user.cartItems);
+            }
+
+            existingItem.quantity = quantity;
+            await user.save();
+            res.json(user.cartItems);
+        } else {
+            res.status(404).json({ message: "Product not found" });
+        }
+    } catch (error) {
+        console.log("Error in update quantity controller: ", error);
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+export const getCartProducts = async (req, res) => {};
