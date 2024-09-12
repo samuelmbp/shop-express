5fbba144e8d5aecba4774e95f50d01f06bdb8350
Samuel Raducan <samuel.thembp@gmail.com>
2024-09-12 11:27:43 +0100

    Implemented remove all from cart router and logic

--- backend/controllers/cart.js
+++ backend/controllers/cart.js
@@ -14,12 +14,25 @@ export const addToCart = async (req, res) => {
         res.json(user.cartItems);
     } catch (error) {
         console.log("Error in add to cart controller: ", error);
-        res.status(500).json({ message: error.message });
+        res.status;
+    }
+};
+
+export const removeAllFromCart = async (req, res) => {
+    try {
+        const { productId } = req.body;
+        const user = req.user;
+
+        if (!productId) user.cartItems = [];
+        else user.cartItems = user.cartItems.filter( (item) => item.id !== productId );
+        
+        await user.save();
+        res.json(user.cartItems);
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
     }
 };
 
 export const getCartProducts = async (req, res) => {};
 
 export const updateQuantity = async (req, res) => {};
-
-export const removeAllFromCart = async (req, res) => {};
