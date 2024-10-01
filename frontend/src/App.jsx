48d146b635f21af14a61d251054877a9aad08e74
Samuel Raducan <samuel.thembp@gmail.com>
2024-10-01 14:30:20 +0100

    Initialised react app with vite and install main packages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={""} />
+            </Routes>
+        </div>
+    );
+};
+
+export default App;
