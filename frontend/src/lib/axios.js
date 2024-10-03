081073add35db808f6c956534a8be8be5982aaf1
Samuel Raducan <samuel.thembp@gmail.com>
2024-10-03 09:50:13 +0100

    Created an axios instance to be reused across the project

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+    baseURL:
+        import.meta.mode === "development"
+            ? "http://localhost:3000/api"
+            : "/api",
+    withCredentials: true, // Send cookies to the server
+});
+
+export default axiosInstance;
